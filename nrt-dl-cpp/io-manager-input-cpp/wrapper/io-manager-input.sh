a9bac4c94e0a8eee058e7a879bacec672de8cc70
rcruzoliver <raul.cruz.oliver@gmail.com>
2024-10-07 10:21:04 +0200

    io manager input

--- nrt-dl-cpp/io-manager-input-cpp/wrapper/io-manager-input.sh
+++ nrt-dl-cpp/io-manager-input-cpp/wrapper/io-manager-input.sh
@@ -9,4 +9,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROS_BASE/usr/include/comm/datalayer/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROS_BASE/usr/lib/$TRIPLET
 source $ROS_BASE/opt/ros/humble/setup.bash #source if ros2 using debian package is build as a snap
 #source $ROS_BASE/setup.bash #source if ros2 is built from source
-source $SNAP/local_setup.bash && $SNAP/app/lib/app/io_manager
+source $SNAP/local_setup.bash && $SNAP/app/lib/app/io_manager_input
